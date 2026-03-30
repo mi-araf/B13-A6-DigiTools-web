3c2f782b9c06436d7100ebd046a8b80681e95d4e
mi-araf <arafmushfiq@gmail.com>
2026-03-30 20:59:30 +0600

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			<h1 className=''>Hello World</h1>
+			<button className='btn btn-xl bg-stone-600'>Hello</button>
+			<p>Yo</p>
+		</>
+	)
+}
+
+export default App
